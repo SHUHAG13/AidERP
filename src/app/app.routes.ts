37755c65084af8f01abb7d4d511473026a74c164
mehedi <mehedi.it@labaidpharma.com>
2024-11-17 16:56:42 +0600

    added error pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LayoutsComponent } from './modules/shared/layouts/layouts.component';
+import { Page404Component } from './modules/shared/errorpages/page404/page404.component';
 
 export const routes: Routes = [
-    { path: '', component: LayoutsComponent}
+    { path: '', component: LayoutsComponent},
+    { path: '**', component: Page404Component },
 ];
