29c2e29bcf64692dfc8ef5d4c158da3198436140
Main Uddin <mainuddin@labaidpharma.com>
2024-12-22 23:55:35 -0800

    passwordPipe.pipe.ts

---
+++ src/app/services/common/pipes/passwordPipe.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'hidePassword',
+  standalone : true
+})
+export class HidePasswordPipe implements PipeTransform {
+  transform(value: string): string {
+    return '*******';
+  }
+}
