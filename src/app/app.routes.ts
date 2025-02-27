eecc9948a9f9ea4ec8222313697d585a3d429b16
mehedi <mehedi.it@labaidpharma.com>
2025-02-27 15:47:37 +0600

    implement interceptor properly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,27 +5,12 @@ import { authGuard } from './guard/auth.guard';
 import { LoginComponent } from './modules/SecurityAdministration/account/login/login.component';
 
 export const routes: Routes = [
-    // { path: 'login', component : LoginComponent},
-    // { 
-    //     path: '', 
-    //     component: LayoutsComponent, 
-    //     canActivate : [authGuard],
-    //     children: [
-    //         { 
-    //             path: 'security/administration/menu/list',
-    //             loadComponent : ()=> import('./modules/securityAdministration/menu/menu/menu.component')
-    //             .then(m => m.MenuComponent) 
-    //         },
-    //         {path:'user',component:UserComponent}
-    //     ]
-    // },
-    // { path: '**', component: Page404Component },
-
     { path: 'login', component : LoginComponent},
     { 
         path: '', 
         component: LayoutsComponent,
         canActivate: [authGuard],
+        //canActivateChild: [authGuard],
         children: [
             { 
                 path: 'security/administration',
